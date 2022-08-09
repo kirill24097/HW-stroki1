001aa21c3ea12d2a4e0d60dd51fe330f9ed3d9e3
kirill24097 <110169552+kirill24097@users.noreply.github.com>
2022-08-09 14:28:50 +0300

    stroki1HomeWork

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        ///1
+        String firstName = "Иван";
+        String lastName = "Иванов";
+        String middleName = "Иванович";
+        String fullName = firstName+ " " + lastName+ " " + middleName+ " ";
+        System.out.println(  "ФИО сотрудника -  " + fullName );
+        ///2
+        System.out.println("Данные ФИО сотрудника для заполнения отчета -  " + fullName.toUpperCase(Locale.ROOT) );
+///3
+        fullName = "Иванов Семён Семёнович";
+        String replaceFullName = fullName.replace("ё" , "е");
+        System.out.println("Данные ФИО ссотрудника - " + replaceFullName);
+
     }
+
+
+
+
 }
